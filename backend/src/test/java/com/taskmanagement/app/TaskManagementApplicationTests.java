a14c572054d549ac0713f1da98a4254e13b5a099
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-06 08:25:57 +0200

    the task management application tests class

---
+++ backend/src/test/java/com/taskmanagement/app/TaskManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.taskmanagement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
