c0363b8d08c70c4d49df7b6e4307758a8ae19182
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-10 08:39:58 +0200

    Modified the task interface

--- backend/src/main/java/com/taskmanagement/app/repository/TaskRepository.java
+++ backend/src/main/java/com/taskmanagement/app/repository/TaskRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     long deleteByCompletedTrue();
+    long deleteByCompletedTrueAndOwnerId(Long ownerId);
 }
