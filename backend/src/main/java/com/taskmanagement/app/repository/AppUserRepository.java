1fcb4fd7827e874a5781b6c7877389193b2f9e6f
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-10 08:39:24 +0200

    Created the app user interface

---
+++ backend/src/main/java/com/taskmanagement/app/repository/AppUserRepository.java
@@ -0,0 +1,10 @@
+package com.taskmanagement.app.repository;
+
+import com.taskmanagement.app.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+}
