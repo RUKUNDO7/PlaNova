8a624d3db713084336c5a5258fc97f31ccd8137a
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-13 11:59:28 +0200

    Modified to add query for tasks without columns

--- backend/src/main/java/com/taskmanagement/app/repository/TaskRepository.java
+++ backend/src/main/java/com/taskmanagement/app/repository/TaskRepository.java
@@ -4,7 +4,10 @@ import com.taskmanagement.app.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     long deleteByCompletedTrue();
     long deleteByCompletedTrueAndOwnerId(Long ownerId);
+    List<Task> findByColumnIsNull();
 }
