4a57799c49755ca474f019db538f3522856d3616
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-13 11:56:37 +0200

    Added task activity queries

---
+++ backend/src/main/java/com/taskmanagement/app/repository/TaskActivityRepository.java
@@ -0,0 +1,10 @@
+package com.taskmanagement.app.repository;
+
+import com.taskmanagement.app.model.TaskActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskActivityRepository extends JpaRepository<TaskActivity, Long> {
+    List<TaskActivity> findByTaskIdOrderByCreatedAtDesc(Long taskId);
+}
