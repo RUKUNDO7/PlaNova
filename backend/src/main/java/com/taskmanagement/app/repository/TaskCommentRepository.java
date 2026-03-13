e92113307b5135d2723a642398f52d705baf5961
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-13 11:58:01 +0200

    Added task comment queries

---
+++ backend/src/main/java/com/taskmanagement/app/repository/TaskCommentRepository.java
@@ -0,0 +1,10 @@
+package com.taskmanagement.app.repository;
+
+import com.taskmanagement.app.model.TaskComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskCommentRepository extends JpaRepository<TaskComment, Long> {
+    List<TaskComment> findByTaskIdOrderByCreatedAtDesc(Long taskId);
+}
