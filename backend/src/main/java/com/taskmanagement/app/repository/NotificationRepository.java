f59774b90b74a94ccbef99e4dd45949fd6b949a3
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-13 11:55:02 +0200

    Added notification queries

---
+++ backend/src/main/java/com/taskmanagement/app/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.taskmanagement.app.repository;
+
+import com.taskmanagement.app.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByRecipientIdOrderByCreatedAtDesc(Long recipientId);
+}
